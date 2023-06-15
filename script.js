9ad327796f36dcc70e2a74547a229f0f4c01dc45
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-06-15 21:10:07 +0200

    Add HTML and CSS for project

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+  const form = document.querySelector("#new-task-form");
+  const input = document.querySelector("#new-task-input");
+  const list_el = document.querySelector()
+})
